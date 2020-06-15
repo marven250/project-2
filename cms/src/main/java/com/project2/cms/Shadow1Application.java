ebe575a5aa5e91f0352658868e8426df48b19411
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-15 13:51:36 -0400

    replacing files

---
+++ cms/src/main/java/com/project2/cms/Shadow1Application.java
@@ -0,0 +1,13 @@
+package com.project2.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shadow1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shadow1Application.class, args);
+	}
+
+}
