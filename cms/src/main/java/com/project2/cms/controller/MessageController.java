c4b20be28f1e86fc4351f617e169c989c9891e33
J-Agens <johntagens@gmail.com>
2020-06-15 18:18:26 -0400

    still working on POST to /messages

--- cms/src/main/java/com/project2/cms/controller/MessageController.java
+++ cms/src/main/java/com/project2/cms/controller/MessageController.java
@@ -1,11 +1,12 @@
 package com.project2.cms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.project2.cms.model.Inbox;
 import com.project2.cms.model.Message;
 import com.project2.cms.services.MessageService;
 
@@ -17,6 +18,8 @@ public class MessageController {
   
   @PostMapping("/messages")
   public Message addNewMessage(@RequestBody Message message) {
+//    Inbox msgInbox = message.getInbox();
+//    message.setInbox(msgInbox);    
     return messageService.createMessage(message);
   }
 
