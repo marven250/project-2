99099047a7eaeaa44075496f16473503cd027aec
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-17 02:11:12 -0400

    Add files via upload
    minor changes

--- cms/src/main/java/com/project2/cms/controller/PostsController.java
+++ cms/src/main/java/com/project2/cms/controller/PostsController.java
@@ -1,5 +1,6 @@
 package com.project2.cms.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.project2.cms.model.Posts;
+import com.project2.cms.model.Writer;
 import com.project2.cms.repository.PostsRepository;
 import com.project2.cms.services.PostsService;
 import com.project2.cms.exception.ResourceNotFoundException;
@@ -40,13 +42,26 @@ public class PostsController {
     public List<Posts> getAllPosts(HttpSession session) {
     	if (session.getAttribute("isLoggedIn") != null
     	        && (Boolean) session.getAttribute("isLoggedIn")) {
-    		if((Integer)session.getAttribute("postpermission") != 1) {
+    		if((Integer)session.getAttribute("writerpermission") != 2) {
     			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
     			}
         return postRepository.findAll();
     	}else{
     	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
+    }
+    
+    @GetMapping("/posts/author/{id}")
+    public Object allPostsByAuthor(@PathVariable(value = "id") Integer authorId,HttpSession session) {
+    	if (session.getAttribute("isLoggedIn") != null
+    	        && (Boolean) session.getAttribute("isLoggedIn")) {
+ 
+      List<Posts> store = postRepository.postsByAuthor(authorId);
+      
+      return store;
+    	}
+	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+
     }
 
     @GetMapping("/posts/{id}")
@@ -55,20 +70,24 @@ public class PostsController {
     	if (session.getAttribute("isLoggedIn") != null
     	        && (Boolean) session.getAttribute("isLoggedIn")) {
     		Posts post = postRepository.findById(postId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    	        return ResponseEntity.ok().body(post);
-    			
-
+    	       Integer key = post.getAuthor(); 
+    	       if((Integer)session.getAttribute("writerpermission") == 2
+       				||
+       				(Integer)session.getAttribute("writerid") == key) {
+    	    	   return ResponseEntity.ok().body(post);  
+    	       }
+    		
+    	       throw new ResponseStatusException(HttpStatus.FORBIDDEN);
     	}else{
   	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
       }
     }
     
-    
+    ///leave for later <when created author is the current id>
     @PostMapping("/posts")
     public Posts createPost(@Validated @RequestBody Posts post, HttpSession session) {
     	if (session.getAttribute("isLoggedIn") != null
-    	        && (Boolean) session.getAttribute("isLoggedIn")) {
-    		
+    	        && (Boolean) session.getAttribute("isLoggedIn")) {		
         return postRepository.save(post);
     	}else{
     	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
@@ -85,56 +104,107 @@ public class PostsController {
     		
 Posts post = postRepository.findById(postId)
     			        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-    			       if(postDetails.getAuthor().toString().trim() != "" && !(postDetails.getAuthor().toString().trim().isEmpty())) { 
-    			    	   post.setAuthor(postDetails.getAuthor());}
-    			       if(postDetails.getPostTitle().toString().trim() != "" && !(postDetails.getPostTitle().toString().trim().isEmpty())) { 
+Integer key = post.getAuthor(); 
+if((Integer)session.getAttribute("writerpermission") == 2
+		||
+		(Integer)session.getAttribute("writerid") == key) {
+	
+			if(postDetails.getPostTitle().toString().trim() != "" && !(postDetails.getPostTitle().toString().trim().isEmpty())) { 
+				
+			
     			    	   post.setPostTitle(postDetails.getPostTitle());}
+    			       //data      
     			       if(postDetails.getPostDescription().toString().trim() != "" && !(postDetails.getPostDescription().toString().trim().isEmpty())) { 
     			    	   post.setPostDescription(postDetails.getPostDescription());}
+    			       //data
     			       if(postDetails.getPostText().toString().trim() != "" && !(postDetails.getPostText().toString().trim().isEmpty())) { 
     			    	   post.setPostText(postDetails.getPostText());}
-    			       if(postDetails.getDateSubmitted().toString().trim() != "" && !(postDetails.getDateSubmitted().toString().trim().isEmpty())) { 
-    			    	   post.setDateSubmitted(postDetails.getDateSubmitted());}
-    			       if(postDetails.getDatePublished().toString().trim() != "" && !(postDetails.getDatePublished().toString().trim().isEmpty())) { 
-    			    	   post.setDatePublished(postDetails.getDatePublished());}
+    			       //data
     			       if(postDetails.getPostType().toString().trim() != "" && !(postDetails.getPostType().toString().trim().isEmpty())) { 
     			    	   post.setPostType(postDetails.getPostType());}
+    			       //data
     			       if(postDetails.getPostField().toString().trim() != "" && !(postDetails.getPostField().toString().trim().isEmpty())) { 
     			    	   post.setPostField(postDetails.getPostField());}
+    			       //data
     			       if(postDetails.getKeyWords().toString().trim() != "" && !(postDetails.getKeyWords().toString().trim().isEmpty())) { 
     			    	   post.setKeyWords(postDetails.getKeyWords());}
-    			       String string1 = postDetails.getResolver().toString();
-    			       try {
-    			           @SuppressWarnings("unused")
-						Double x = Double.parseDouble(string1);
-    			           post.setResolver(postDetails.getResolver());
-    			       } catch (NumberFormatException e) {
-    			           e.getMessage();
-    			       }
-    			       if(postDetails.getResolver().toString().trim() != "" && !(postDetails.getResolver().toString().trim().isEmpty())) { 
-    			    	   post.setResolver(postDetails.getResolver());}
-    			       
-    			       if(postDetails.getStatus().toString().trim() != "" && !(postDetails.getStatus().toString().trim().isEmpty())) { 
-    			    	   post.setStatus(postDetails.getStatus());}
-    			       
-    			       String string2 = postDetails.getPublished().toString();
-    			       try {
-    			           @SuppressWarnings("unused")
-						Double x = Double.parseDouble(string2);
-    			           post.setPublished(postDetails.getPublished());
-    			       } catch (NumberFormatException e) {
-    			           e.getMessage();
-    			       }
-    			       if(postDetails.getResolver().toString().trim() != "" && !(postDetails.getPublished().toString().trim().isEmpty())) { 
-    			    	   post.setPublished(postDetails.getPublished());}
-    			       
     			       
     			       final Posts updatedPost = postRepository.save(post);
-    			        return ResponseEntity.ok(updatedPost);
-    			
+    			        return ResponseEntity.ok(updatedPost); 
+					}
+					throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    	}else{
+  	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+      }
+    }
+    
+    //publish
+    @PutMapping("/publish/{id}")
+    public ResponseEntity<Posts> publishPost(@PathVariable(value = "id") Integer postId
+    		,HttpSession session) throws ResourceNotFoundException {
+    	if ((session.getAttribute("isLoggedIn") != null
+    	        && (Boolean) session.getAttribute("isLoggedIn"))
+    	        && (Integer)session.getAttribute("writerpermission") == 2) {
+    		
+    					Posts post = postRepository.findById(postId)
+    			        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+					  //data
+    					java.util.Date myDate = new Date();
+    					java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
 
-    	
+    			    	   post.setResolver((Integer)session.getAttribute("writerid"));
+    			    	   post.setPublished(1);
+    			    	   post.setDatePublished(sqlDate);
+
+    			       //ends	       
+    			       final Posts updatedPost = postRepository.save(post);
+    			        return ResponseEntity.ok(updatedPost);    			 	
+    	}else{
+  	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+      }
+    }
+    
+    //UNPUBLISH
+    @PutMapping("/unpublish/{id}")
+    public ResponseEntity<Posts> unpublishPost(@PathVariable(value = "id") Integer postId
+    		,HttpSession session) throws ResourceNotFoundException {
+    	if ((session.getAttribute("isLoggedIn") != null
+    	        && (Boolean) session.getAttribute("isLoggedIn"))
+    	        && (Integer)session.getAttribute("writerpermission") == 2) {
+    		
+    					Posts post = postRepository.findById(postId)
+    			        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+					  //data
+						java.util.Date myDate = new Date();
+    					java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+
+    			    	   post.setResolver((Integer)session.getAttribute("writerid"));
+    			    	   post.setPublished(0);
+    			    	   post.setDatePublished(sqlDate);
+    			       //ends	       
+    			       final Posts updatedPost = postRepository.save(post);
+    			        return ResponseEntity.ok(updatedPost);    			 	
+    	}else{
+  	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+      }
+    }
+    //change status
+    @PutMapping("/status/{status}/{id}")
+    public ResponseEntity<Posts> statusPost(@PathVariable(value = "id") Integer postId
+    		,@PathVariable(value = "status") String status
+    		,HttpSession session) throws ResourceNotFoundException {
+    	if ((session.getAttribute("isLoggedIn") != null
+    	        && (Boolean) session.getAttribute("isLoggedIn"))
+    	        && (Integer)session.getAttribute("writerpermission") == 2) {
+    		
+    					Posts post = postRepository.findById(postId)
+    			        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+					  //data
+    			    	   post.setResolver((Integer)session.getAttribute("writerid"));
+    			    	   post.setStatus(status);
+    			       //ends	       
+    			       final Posts updatedPost = postRepository.save(post);
+    			        return ResponseEntity.ok(updatedPost);    			 	
     	}else{
   	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
       }
@@ -145,6 +215,9 @@ Posts post = postRepository.findById(postId)
          throws ResourceNotFoundException {
     	if (session.getAttribute("isLoggedIn") != null
     	        && (Boolean) session.getAttribute("isLoggedIn")) {
+    		if((Integer)session.getAttribute("writerpermission") != 2) {
+    			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    			}
     	Posts post = postRepository.findById(postId)
        .orElseThrow(() -> new ResourceNotFoundException("Post not found for this id :: " + postId));
 
@@ -152,6 +225,7 @@ Posts post = postRepository.findById(postId)
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
+        
     	}else{
     	      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
