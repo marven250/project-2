fe74d7b0ae200b6097e0826a0f875d170314beb9
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2020-06-17 02:14:43 -0400

    Add files via upload
    postsByAuthor -- added

--- cms/src/main/java/com/project2/cms/repository/PostsRepository.java
+++ cms/src/main/java/com/project2/cms/repository/PostsRepository.java
@@ -1,8 +1,11 @@
 package com.project2.cms.repository;
 
+import java.util.List;
+
 //import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +16,9 @@ import com.project2.cms.model.Posts;
 public interface PostsRepository extends JpaRepository<Posts, Integer>{
 
 	
-//	  @Query(value = "select * from project2.writers where username = :username and password = :password",
-//		      nativeQuery = true)
-//		  List<Writer> checkUsernamePassword(String username, String password);
+	  @Query(value = "select * from project2.posts where author = :author",
+		      nativeQuery = true)
+		  List<Posts> postsByAuthor(Integer author );
 	  
 	  
 }
